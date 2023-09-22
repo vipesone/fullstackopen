56001085d9ce5c1542973f0bc62861ca65a01bd8
Ville Pesonen <ville.pesonen@siili.com>
2023-09-22 14:46:10 +0300

    Handle notifications and add some style to the application

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,16 @@ import { useState, useEffect } from 'react'
 
 import Contact from './components/Contact'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
 import Form from './components/Form'
 
 import personService from './services/persons'
 
 function App() {
+  // Variables for notifications.
+  const [notificationMessage, setNotificationMessage] = useState(null);
+  const [notificationStatus, setNotificationStatus] = useState(null);
+
   // Variables for filter.
   const [filter, setFilter] = useState('')
 
@@ -54,8 +59,23 @@ function App() {
         .create(newPerson)
         .then(response => {
           setPersons(persons.concat(response.data))
+          addTemporaryNotification(
+            `${response.data.name} was added`,
+            'notification'
+          )
         })
     }
+  }
+
+  // Set up dissappearing notification.
+  const addTemporaryNotification = (message, status) => {
+    setNotificationMessage(message)
+    setNotificationStatus(status)
+
+    setTimeout(() => {
+      setNotificationMessage(null)
+      setNotificationStatus(null)
+    }, 3000)
   }
 
   // Handle person removal button click.
@@ -64,7 +84,16 @@ function App() {
     if (window.confirm(`Are you sure you want to remove ${person.name}?`)) {
       personService
         .remove(id)
-        .then(() => setPersons(persons.filter((person) => person.id != id)))
+        .then(() => {
+          setPersons(persons.filter((person) => person.id != id))
+          addTemporaryNotification(
+            `${person.name} was removed`,
+            'notification'
+          )
+        })
+        .catch(error => {
+          addTemporaryNotification(`${person.name} has already been removed from the phonebook`, 'error')
+        })
     }
   }
 
@@ -82,6 +111,7 @@ function App() {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notificationMessage} status={notificationStatus} />
       <Filter handleFilterChange={handleFilterChange} />
 
       <h2>add a new</h2>
