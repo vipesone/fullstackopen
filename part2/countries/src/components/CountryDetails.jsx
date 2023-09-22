3c089032873965921c103988d0ac457fe50f0483
Ville Pesonen <ville.pesonen@siili.com>
2023-09-22 17:26:33 +0300

    Add missing details for countries exercise

--- part2/countries/src/components/CountryDetails.jsx
+++ part2/countries/src/components/CountryDetails.jsx
@@ -3,12 +3,14 @@ const CountryDetails = ({country}) => {
     <div>
       <h2>{country.name.common}</h2>
       <div>capital {country.capital}</div>
+      <div>area {country.area}</div>
       <strong>languages:</strong>
       <ul>{
         Object.keys(country.languages).map((key, index) => {
           return <li key={key}>{country.languages[key]}</li>
         })
       }</ul>
+      <div> <img src={country.flags.svg} width={200} /></div>
     </div>
   )
 }
