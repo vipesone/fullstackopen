e6007adc05ce89ef23cb7d16fb380b5204812e25
Ville Pesonen <ville.pesonen@siili.com>
2023-09-22 12:52:06 +0300

    Utilize JSON server for person data

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Contact from './components/Contact'
 import Filter from './components/Filter'
@@ -13,12 +14,16 @@ function App() {
   const [newNumber, setNewNumber] = useState('')
 
   // Set up list of persons.
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
+  // Fetch phonebook.
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   // Handle filtering persons based on user input.
   const visiblePersons = (filter == '')
