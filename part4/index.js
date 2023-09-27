5344483c139b5eef6f135bf85bb9e43d254b2fb3
Ville Pesonen <ville.pesonen@siili.com>
2023-09-27 08:40:30 +0300

    Separate application logic into separate file

--- part4/index.js
+++ part4/index.js
@@ -1,20 +1,7 @@
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
+const app = require('./app')
 const config = require('./utils/config')
-const {info} = require('./utils/logger')
-
-const blogsRouter = require('./controller/blogs')
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
+const { info } = require('./utils/logger')
 
 const PORT = config.PORT
 app.listen(PORT, () => {
