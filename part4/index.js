4d0a548c4316279029fd8d7f4f6ce0fb9a0a555e
Ville Pesonen <ville.pesonen@siili.com>
2023-09-27 08:33:36 +0300

    Separate controller

--- part4/index.js
+++ part4/index.js
@@ -3,32 +3,18 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+
 const config = require('./utils/config')
 const {info} = require('./utils/logger')
+
+const blogsRouter = require('./controller/blogs')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
