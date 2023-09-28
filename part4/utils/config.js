51042b2d20f9d300c9caa9688c1a644de513ebf7
Ville Pesonen <ville.pesonen@siili.com>
2023-09-28 12:41:59 +0300

    Configure application for testing

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 
-let MONGODB_URI = process.env.MONGODB_URI
-let PORT = 3003
+
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.MONGODB_URI_TEST
+  : process.env.MONGODB_URI
+const PORT = 3003
 
 module.exports = {
   MONGODB_URI,
