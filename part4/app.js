4f17bb3781f46373c5c8dfa206f3c6ade9fe6472
Ville Pesonen <ville.pesonen@siili.com>
2023-09-29 11:00:57 +0300

    Rename middleware

--- part4/app.js
+++ part4/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const config = require('./utils/config')
-const middleWare = require('./utils/middleware')
+const middleware = require('./utils/middleware')
 
 const usersRouter = require('./controller/users')
 const loginRouter = require('./controller/login')
@@ -16,12 +16,13 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
-app.use(middleWare.tokenExtractor)
+app.use(middleware.tokenExtractor)
+
 
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 
-app.use(middleWare.errorHandler)
+app.use(middleware.errorHandler)
 
 module.exports = app
