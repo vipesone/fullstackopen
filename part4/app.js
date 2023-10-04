93c8340097a39f1099d3f0c3050851fed069cab6
Ville Pesonen <ville.pesonen@siili.com>
2023-10-04 09:20:09 +0300

    Add testing controller to backend (part 4)

--- part4/app.js
+++ part4/app.js
@@ -23,6 +23,11 @@ app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controller/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.errorHandler)
 
 module.exports = app
