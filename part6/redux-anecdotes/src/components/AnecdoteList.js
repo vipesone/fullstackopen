f0b38256b355698a271b482ea76de2090bd77ece
Ville Pesonen <ville.pesonen@siili.com>
2023-10-05 14:50:50 +0300

    Add filtering to list of anecdotes

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,15 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+
+    if (filter === '') {
+      return anecdotes
+    } else {
+      return anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter))
+    }
+  })
+
   const dispatch = useDispatch()
 
   const vote = (id) => {
